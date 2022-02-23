e8633ced77a7e93fcb6fe99242ca3bc50d835976
Valentin Pheulpin <Valentin.Pheulpin.Pro@gmail.com>
2022-02-23 17:54:43 +0100

    Settings Updated

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,4 @@
 import os
-# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,5 +110,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# django_heroku.settings(locals())
