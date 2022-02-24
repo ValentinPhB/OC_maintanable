828054a31423a6395fb966c000593181edb456c7
Valentin Pheulpin <Valentin.Pheulpin.Pro@gmail.com>
2022-02-24 11:55:20 +0100

    Sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,9 +5,13 @@ from django.conf import settings
 
 from . import views
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls', namespace='letting')),
     path('profiles/', include('profiles.urls', namespace='profile')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
