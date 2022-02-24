086def202c3e937eb4ec201cf19fde5ec192ee3a
Valentin Pheulpin <Valentin.Pheulpin.Pro@gmail.com>
2022-02-24 11:58:55 +0100

    Sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,9 +5,11 @@ from django.conf import settings
 
 from . import views
 
+
 def trigger_error(request):
-    division_by_zero = 1 / 0
-    
+    1 / 0
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls', namespace='letting')),
